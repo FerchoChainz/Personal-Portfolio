757ba81744738260e84c13a12fd20ae101cb038b
FerchoChainz <a21110132@ceti.mx>
2026-01-19 11:40:34 -0600

    Implementing HOC

--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -1,6 +1,7 @@
 import useWindowStore from '#store/window'
 import { useGSAP } from '@gsap/react';
 import React, { useLayoutEffect, useRef } from 'react'
+import gsap from 'gsap';
 
 const WindowWrapper = (Component, windowKey) => {
 
@@ -15,6 +16,11 @@ const WindowWrapper = (Component, windowKey) => {
             if(!el || !isOpen) return;
 
             el.style.display = "block";
+
+            gsap.fromTo(el, 
+                {scale: 0.8,opacity:0, y:40},
+                {scale: 1, opacity:1, y:0, duration:0.4, ease:'power3.out'})
+
         },[isOpen])
 
         useLayoutEffect(()=>{
