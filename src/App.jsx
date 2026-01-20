9ee6f7bcb109a45063fa8bcbbb3cb8cff9d31438
FerchoChainz <a21110132@ceti.mx>
2026-01-20 15:32:28 -0600

    Text files and images showed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { gsap } from "gsap/gsap-core";
 import { Draggable } from "gsap/all";
 
 import { Navbar, Welcome, Dock } from "#components";
-import { Terminal, Safari, Resume, Finder } from "#windows";
+import { Terminal, Safari, Resume, Finder, Text, Image } from "#windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -17,6 +17,8 @@ const App = ()=> {
       <Safari/>
       <Resume/>
       <Finder/>
+      <Text/>
+      <Image/>
     </main>
   )
 }
