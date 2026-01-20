237b34a9b01156d61619cd69e8c175192759cf3b
FerchoChainz <a21110132@ceti.mx>
2026-01-20 16:59:04 -0600

    TODO: the gallery section

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Navbar from "./Navbar";
 import Welcome from "./Welcome";
 import Dock from "./Dock";
+import Home from "./Home";
 import WindowControls from "./WindowControls";
 import Safari from "#windows/Safari";
 import { Resume } from "#windows";
-export {Navbar, Welcome, Dock,  WindowControls, Safari, Resume}
+export {Navbar, Welcome, Dock,  WindowControls, Safari, Resume, Home}
